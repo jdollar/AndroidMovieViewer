26a3e6e891e9db96a192d7e46dbc34d6f97e13fb
jdollar <dollarhidejoel@gmail.com>
2015-12-06 11:53:01 -0500

    Finishing added tests for new authentication token and doing refactoring

--- movieViewer/src/test/java/org/dollarhide/androidmovieviewer/service/AuthenticationServiceTest.java
+++ movieViewer/src/test/java/org/dollarhide/androidmovieviewer/service/AuthenticationServiceTest.java
@@ -2,68 +2,90 @@ package org.dollarhide.androidmovieviewer.service;
 
 import org.apache.http.client.methods.HttpGet;
 import org.dollarhide.androidmovieviewer.model.RestResult;
-import org.dollarhide.androidmovieviewer.util.LoggingUtil;
 import org.dollarhide.androidmovieviewer.util.ResourcePropertyReader;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.Properties;
 
-import static org.junit.Assert.fail;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.dollarhide.androidmovieviewer.service.AuthenticationService.*;
+import static org.dollarhide.androidmovieviewer.util.LoggingUtil.DEBUG_ENABLED_PARAM;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest extends BaseServiceTestCase{
-
     private static final String TAG = "AuthenticationServiceTest";
 
     @Test
-    public void testNewAuthenticationToken_successWithData() {
+    public void testNewAuthenticationToken_successWithData() throws IOException {
         String expectedData = "requestTokenTest";
-        JSONObject expectedJSONObject = new JSONObject();
+        RestResult expectedRestResult = new RestResult(true, expectedData);
+        JSONObject expectedJSONObject = setupJSONData(true, REQUEST_TOKEN, expectedData);
 
-        try {
-            expectedJSONObject.put(AuthenticationService.SUCCESS_PARAM, true);
-            expectedJSONObject.put(AuthenticationService.REQUEST_TOKEN, expectedData);
-
-            Properties testApiProperties = new Properties();
-            testApiProperties.put(AuthenticationService.AUTHENTICATION_NEW_PARAM, "testUrl{0}");
-            testApiProperties.put(LoggingUtil.DEBUG_ENABLED_PARAM, "false");
-            ResourcePropertyReader.setApiProperties(testApiProperties);
-            when(mockHttpClient.execute(any(HttpGet.class))).thenReturn(mockHttpResponse);
-            when(mockStatusLine.getStatusCode()).thenReturn(200);
-            when(mockHttpEntity.getContent())
-                    .thenReturn(new ByteArrayInputStream(expectedJSONObject.toString().getBytes(StandardCharsets.UTF_8)));
-
-            RestResult result = getService().getNewAuthenticationToken();
-            assert result.getSuccessFlag();
-            assert expectedData.equals(result.getData());
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Exception has occurred when it should not.");
-        }
+        RestResult result = newAuthenticationTokenTest_common(200, expectedJSONObject);
+        assertRestResult(expectedRestResult, result);
     }
 
     @Test
-    public void testNewAuthenticiationToken_successWithBlankData() {
-        assert false;
+    public void testNewAuthenticiationToken_successWithBlankData() throws IOException {
+        String expectedData = "";
+        RestResult expectedRestResult = new RestResult(true, expectedData);
+        JSONObject expectedJSONObject = setupJSONData(true, REQUEST_TOKEN, expectedData);
+
+        RestResult result = newAuthenticationTokenTest_common(200, expectedJSONObject);
+        assertRestResult(expectedRestResult, result);
     }
 
     @Test
     public void testNewAuthenticiationToken_badRequestCode() {
-        assert false;
+        RestResult expectedRestResult = new RestResult(false, null);
+        RestResult result = newAuthenticationTokenTest_common(403, null);
+        assertRestResult(expectedRestResult, result);
     }
 
     @Test
-    public void testNewAuthenticiationToken_failureWithData() {
-        assert false;
+    public void testNewAuthenticiationToken_failureWithData() throws IOException {
+        String expectedData = "Bad Data";
+        RestResult expectedRestResult = new RestResult(false, expectedData);
+        JSONObject expectedJSONObject = setupJSONData(false, REQUEST_TOKEN, expectedData);
+
+        RestResult result = newAuthenticationTokenTest_common(200, expectedJSONObject);
+        assertRestResult(expectedRestResult, result);
     }
 
     @Test
-    public void testNewAuthenticiationToken_failureBlankData() {
-        assert false;
+    public void testNewAuthenticiationToken_failureBlankData() throws IOException {
+        String expectedData = "";
+        RestResult expectedRestResult = new RestResult(false, expectedData);
+        JSONObject expectedJSONObject = setupJSONData(false, REQUEST_TOKEN, expectedData);
+
+        RestResult result = newAuthenticationTokenTest_common(200, expectedJSONObject);
+        assertRestResult(expectedRestResult, result);
+    }
+
+    private RestResult newAuthenticationTokenTest_common(int statusCode, JSONObject expectedJSONObject) {
+        try {
+            Properties testApiProperties = setupPropertiesData(AUTHENTICATION_NEW_PARAM, "testUrl{0}");
+            ResourcePropertyReader.setApiProperties(testApiProperties);
+
+            when(mockHttpClient.execute(any(HttpGet.class))).thenReturn(mockHttpResponse);
+            when(mockStatusLine.getStatusCode()).thenReturn(statusCode);
+            if (expectedJSONObject != null) {
+                when(mockHttpEntity.getContent())
+                        .thenReturn(new ByteArrayInputStream(expectedJSONObject.toString().getBytes(UTF_8)));
+            }
+
+            return getService().getNewAuthenticationToken();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Exception has occurred when it should not.");
+        }
+
+        return null;
     }
 
     @Override
@@ -76,4 +98,25 @@ public class AuthenticationServiceTest extends BaseServiceTestCase{
         return (AuthenticationService) super.getService();
     }
 
+    private JSONObject setupJSONData(boolean successValue, String dataName, String expectedData) {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put(SUCCESS_PARAM, successValue);
+            jsonObject.put(dataName, expectedData);
+            return jsonObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Exception occurred creating JSON test data");
+        }
+
+        return jsonObject;
+    }
+
+    private Properties setupPropertiesData(String serviceUrlParam, String serviceUrl) {
+        Properties testApiProperties = new Properties();
+        testApiProperties.put(serviceUrlParam, serviceUrl);
+        testApiProperties.put(DEBUG_ENABLED_PARAM, "false");
+        return testApiProperties;
+    }
 }
--- movieViewer/src/test/java/org/dollarhide/androidmovieviewer/service/BaseServiceTestCase.java
+++ movieViewer/src/test/java/org/dollarhide/androidmovieviewer/service/BaseServiceTestCase.java
@@ -4,11 +4,15 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.dollarhide.androidmovieviewer.model.RestResult;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -35,6 +39,24 @@ public abstract class BaseServiceTestCase {
 
         when(mockHttpResponse.getEntity()).thenReturn(mockHttpEntity);
         when(mockHttpResponse.getStatusLine()).thenReturn(mockStatusLine);
+    }
+
+    protected void assertRestResult(RestResult expectedResult, RestResult actualResult) {
+        if (expectedResult == null) {
+            assertNull(actualResult);
+            return;
+        }
+
+        assertNotNull(actualResult);
+        assertEquals(expectedResult.getSuccessFlag(), actualResult.getSuccessFlag());
+
+        if (expectedResult.getData() == null) {
+            assertNull(actualResult.getData());
+            return;
+        }
+
+        assertNotNull(actualResult.getData());
+        assertEquals(expectedResult.getData(), actualResult.getData());
     }
 
     protected abstract BaseService createService();
