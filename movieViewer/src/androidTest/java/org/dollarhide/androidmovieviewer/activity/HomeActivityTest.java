cb6c366943af2332d8f0fc61a8384e80b87f27e9
jdollar <dollarhidejoel@gmail.com>
2015-12-08 01:27:35 -0500

    Fixed issues on instrumentation test for home acttivity and added in a few tests for the home activity

--- movieViewer/src/androidTest/java/org/dollarhide/androidmovieviewer/activity/HomeActivityTest.java
+++ movieViewer/src/androidTest/java/org/dollarhide/androidmovieviewer/activity/HomeActivityTest.java
@@ -1,17 +1,20 @@
 package org.dollarhide.androidmovieviewer.activity;
 
-import android.content.Intent;
-import android.test.ActivityUnitTestCase;
-import android.test.UiThreadTest;
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.test.ViewAsserts;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.view.ContextThemeWrapper;
+import android.view.View;
 import android.widget.Button;
 import org.dollarhide.androidmovieviewer.movieviewer.R;
 
-public class HomeActivityTest extends ActivityUnitTestCase<Home> {
+public class HomeActivityTest extends ActivityInstrumentationTestCase2<Home> {
 
     private Home homeActivityTest;
-    private Intent homeIntent;
+    private Button searchButton;
+    private Button loginButton;
+    private Button settingsButton;
 
     public HomeActivityTest() {
         super(Home.class);
@@ -20,38 +23,39 @@ public class HomeActivityTest extends ActivityUnitTestCase<Home> {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        homeIntent = new Intent(getInstrumentation().getTargetContext(), Home.class);
-        /*MovieViewApplication application = new MovieViewApplication();
-        application.setTheme(R.style.Theme_AppCompat);
-        setApplication(application);*/
-        ContextThemeWrapper context = new ContextThemeWrapper(getInstrumentation().getTargetContext(), R.style.Theme_AppCompat);
-
-        setActivityContext(context);
-        setActivity(launchActivity("org.dollarhide.androidmovieviewer.movieviewer", Home.class, null));
-        //startActivity(homeIntent, null, null);
+        setActivityInitialTouchMode(true);
         homeActivityTest = getActivity();
+        searchButton = (Button) homeActivityTest.findViewById(R.id.search_button);
+        loginButton = (Button) homeActivityTest.findViewById(R.id.login_button_home);
+        settingsButton = (Button) homeActivityTest.findViewById(R.id.settings_button);
+    }
+
+    @MediumTest
+    public void testHomeActivity_checkViewGenerations() {
+        homeActivityTest = getActivity();
+        final View decorView = homeActivityTest.getWindow().getDecorView();
+        ViewAsserts.assertOnScreen(decorView, searchButton);
+        ViewAsserts.assertOnScreen(decorView, settingsButton);
+        ViewAsserts.assertOnScreen(decorView, loginButton);
     }
 
     @MediumTest
     public void testHomeActivity_openSearchStartsSearchActivity() {
-        Button searchButton = (Button) getActivity().findViewById(R.id.search_button);
-        searchButton.performClick();
+        Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(SearchActivity.class.getName(), null, false);
+        TouchUtils.clickView(this, searchButton);
 
-        final Intent launchedIntent = getStartedActivityIntent();
-        assertNotNull(launchedIntent);
-        assertEquals(SearchActivity.class.getName(), launchedIntent.getComponent().getClassName());
-        getActivity().finish();
+        SearchActivity searchActivity = (SearchActivity) getInstrumentation().waitForMonitorWithTimeout(monitor, 3);
+        assertNotNull(searchActivity);
+        searchActivity.finish();
     }
 
-    @UiThreadTest
     @MediumTest
     public void testHomeActivity_openLoginStartsLoginActivity() {
-        Button loginButton = (Button) getActivity().findViewById(R.id.login_button_home);
-        loginButton.performClick();
+        Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(LoginActivity.class.getName(), null, false);
+        TouchUtils.clickView(this, loginButton);
 
-        final Intent launchedIntent = getStartedActivityIntent();
-        assertNotNull(launchedIntent);
-        assertEquals(LoginActivity.class.getName(), launchedIntent.getComponent().getClassName());
-        getActivity().finish();
+        LoginActivity loginActivity = (LoginActivity) getInstrumentation().waitForMonitorWithTimeout(monitor, 3);
+        assertNotNull(loginActivity);
+        loginActivity.finish();
     }
 }
